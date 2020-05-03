54a96ac07d52a27cd23fc4bffb90cf211ebff836
thenab0303 <thenab0303@gmail.com>
2020-05-03 17:39:05 -0400

    added Discord custom block

---
+++ CustomBlocks/Discord_Invite.js
@@ -0,0 +1,7 @@
+const discordContainer = document.querySelector(".discordContainer");
+
+bannerContainer.innerHTML = `
+    <a href="https://discord.gg/SkyrimTogether" target="_blank">
+        <div class="discordImage"></div>
+    </a>
+`;
