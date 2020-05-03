a9be331c22ef4ea4da1accc01dfe5378e203494e
thenab0303 <thenab0303@gmail.com>
2020-05-02 22:06:28 -0400

    removed video

--- CustomBlocks/Download_Banner.js
+++ CustomBlocks/Download_Banner.js
@@ -1,6 +1,5 @@
 const bannerContainer = document.querySelector(".bannerContainer");
 
 bannerContainer.innerHTML = `
-    <video src="https://thenab0303.github.io/skyrimtogether/vidoes/videobg3.mp4" muted autoplay></video>
     <div class="bannerImage"></div>
 `;
