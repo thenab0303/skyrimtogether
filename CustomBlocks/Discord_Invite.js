d67db01f76788920ec0ce87c1800ff8aebc452f8
thenab0303 <thenab0303@gmail.com>
2020-05-03 17:47:17 -0400

    changed bannerContainer to discordContainer in Discord_Invite.js

--- CustomBlocks/Discord_Invite.js
+++ CustomBlocks/Discord_Invite.js
@@ -1,6 +1,6 @@
 const discordContainer = document.querySelector(".discordContainer");
 
-bannerContainer.innerHTML = `
+discordContainer.innerHTML = `
     <a href="https://discord.gg/SkyrimTogether" target="_blank">
         <div class="discordImage"></div>
     </a>
