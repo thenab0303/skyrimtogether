9766f3128907672ac0840862c724ebbab98f0ae5
thenab0303 <thenab0303@gmail.com>
2020-05-03 22:00:31 -0400

    aligned server stats custom block

--- CustomBlocks/Server_Stats.js
+++ CustomBlocks/Server_Stats.js
@@ -2,10 +2,14 @@ const statsContainer = document.querySelector(".statsContainer");
 const url = "https://list.skyrim-together.com/stat";
 
 statsContainer.innerHTML = `
-    <div class="currentPlayers">N/A</div>
-    <div class="currentServers">N/A</div>
-    <div class="maxPlayers">N/A</div>
-    <div class="maxServers">N/A</div>
+    <div class="statsCurrent">
+        <div class="currentPlayers">N/A</div>
+        <div class="currentServers">N/A</div>
+    </div>
+    <div class="statsMax">
+        <div class="maxPlayers">N/A</div>
+        <div class="maxServers">N/A</div>
+    </div>
 `;
 
 fetch(url)
