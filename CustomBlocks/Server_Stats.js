51a0e03108e349d04d0fd39e0231107aff2fc5b1
thenab0303 <thenab0303@gmail.com>
2020-05-03 21:36:49 -0400

    added placeholder values for server stats

--- CustomBlocks/Server_Stats.js
+++ CustomBlocks/Server_Stats.js
@@ -1,7 +1,16 @@
 const statsContainer = document.querySelector(".statsContainer");
 const url = "https://list.skyrim-together.com/stat";
 
+statsContainer.innerHTML = `
+    <div class="currentPlayers">N/A</div>
+    <div class="currentServers">N/A</div>
+    <div class="maxPlayers">N/A</div>
+    <div class="maxServers">N/A</div>
+`;
+
 fetch(url)
 .then(resp => resp.json())
-.then(data => console.log(data))
+.then(data => {
+    console.log(data);
+})
 .catch((error) => console.error("Error:", error));
