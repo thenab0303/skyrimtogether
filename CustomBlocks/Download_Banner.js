45d3c767d2574924b13fbcfce7f6cf0f805debc9
thenab0303 <thenab0303@gmail.com>
2020-05-07 00:03:37 -0400

    adding download button image tags

--- CustomBlocks/Download_Banner.js
+++ CustomBlocks/Download_Banner.js
@@ -1,5 +1,7 @@
 const bannerContainer = document.querySelector(".bannerContainer");
 
 bannerContainer.innerHTML = `
-    <div class="bannerImage"></div>
+    <div class="bannerImage">
+        <a href="#"><div class="bannerDownload"></div></a>
+    </div>
 `;
