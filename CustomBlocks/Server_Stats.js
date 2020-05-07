0a1db2647dafd9e5ff632898943c1c1891526299
thenab0303 <thenab0303@gmail.com>
2020-05-06 22:20:04 -0400

    added linear gradient to borders of stats code block

--- CustomBlocks/Server_Stats.js
+++ CustomBlocks/Server_Stats.js
@@ -3,12 +3,12 @@ const url = "https://list.skyrim-together.com/stat";
 
 statsContainer.innerHTML = `
     <div class="statsCurrent">
-        <div class="currentPlayers"><div id="currentPlayerNumbers">N/A</div><div class="statsName">Current Players</div></div>
-        <div class="currentServers"><div id="currentServerNumbers">N/A</div><div class="statsName">Current Servers</div></div>
+        <div class="currentPlayers"><div id="currentPlayerNumbers">N/A</div><div class="statsName">Players Active</div></div>
+        <div class="currentServers"><div id="currentServerNumbers">N/A</div><div class="statsName">Servers Active</div></div>
     </div>
     <div class="statsMax">
-        <div class="maxPlayers"><div id="maxPlayerNumbers">N/A</div><div class="statsName">Max Players</div></div>
-        <div class="maxServers"><div id="maxServerNumbers">N/A</div><div class="statsName">Max Servers</div></div>
+        <div class="maxPlayers"><div id="maxPlayerNumbers">N/A</div><div class="statsName">Players Total</div></div>
+        <div class="maxServers"><div id="maxServerNumbers">N/A</div><div class="statsName">Servers Total</div></div>
     </div>
 `;
 
