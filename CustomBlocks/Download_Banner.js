de69247677ffe1c46436ae5bce24d06602dee909
thenab0303 <thenab0303@gmail.com>
2020-05-07 16:09:28 -0400

    added link to download for download button

--- CustomBlocks/Download_Banner.js
+++ CustomBlocks/Download_Banner.js
@@ -6,7 +6,7 @@ bannerContainer.innerHTML = `
             <div class="bannerSkyrim">SKYRIM</div>
             <div class="bannerDivider"></div>
             <div class="bannerTogether">TOGETHER</div>
-            <a href="#"><div class="bannerDownloadButton"></div></a>
+            <a href="https://github.com/SkyrimTogether/issues-launcher/releases/" target="_blank"><div class="bannerDownloadButton"></div></a>
         </div>
     </div>
 `;
