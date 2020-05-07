57e8213b137718be8bbe6d9eca254f1ea2b09f11
thenab0303 <thenab0303@gmail.com>
2020-05-06 22:06:38 -0400

    added names to stats boxes

--- CustomBlocks/Server_Stats.js
+++ CustomBlocks/Server_Stats.js
@@ -3,12 +3,12 @@ const url = "https://list.skyrim-together.com/stat";
 
 statsContainer.innerHTML = `
     <div class="statsCurrent">
-        <div class="currentPlayers">N/A</div>
-        <div class="currentServers">N/A</div>
+        <div class="currentPlayers"><div id="currentPlayerNumbers">N/A</div><div class="statsName">Current Players</div></div>
+        <div class="currentServers"><div id="currentServerNumbers">N/A</div><div class="statsName">Current Servers</div></div>
     </div>
     <div class="statsMax">
-        <div class="maxPlayers">N/A</div>
-        <div class="maxServers">N/A</div>
+        <div class="maxPlayers"><div id="maxPlayerNumbers">N/A</div><div class="statsName">Max Players</div></div>
+        <div class="maxServers"><div id="maxServerNumbers">N/A</div><div class="statsName">Max Servers</div></div>
     </div>
 `;
 
