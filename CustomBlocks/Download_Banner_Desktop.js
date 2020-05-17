d210eb32376a2729004ca8c742ac7c32168ebb11
thenab0303 <thenab0303@gmail.com>
2020-05-17 08:55:57 -0400

    fixed scaling issue for banner text

--- CustomBlocks/Download_Banner_Desktop.js
+++ CustomBlocks/Download_Banner_Desktop.js
@@ -3,10 +3,10 @@ const bannerContainerDesktop = document.querySelector(".bannerContainerDesktop")
 bannerContainerDesktop.innerHTML = `
     <div class="bannerImage bannerImageDesktop">
         <div class="bannerText bannerTextDesktop">
-            <div class="bannerSkyrim">SKYRIM</div>
-            <div class="bannerDivider"></div>
-            <div class="bannerTogether">TOGETHER</div>
-            <a href="https://github.com/SkyrimTogether/issues-launcher/releases/" target="_blank"><div class="bannerDownloadButton"></div></a>
+            <div class="bannerSkyrim bannerTextDesktopScale">SKYRIM</div>
+            <div class="bannerDivider bannerTextDesktopScale"></div>
+            <div class="bannerTogether bannerTextDesktopScale">TOGETHER</div>
+            <a href="https://github.com/SkyrimTogether/issues-launcher/releases/" target="_blank"><div class="bannerDownloadButton bannerTextDesktopScale"></div></a>
         </div>
     </div>
 `;
