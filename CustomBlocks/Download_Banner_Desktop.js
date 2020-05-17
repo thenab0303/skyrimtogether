d50c374503375f090daaf76c2a12e18fb827c229
thenab0303 <thenab0303@gmail.com>
2020-05-17 08:59:14 -0400

    fixed divider spacing in banner text

--- CustomBlocks/Download_Banner_Desktop.js
+++ CustomBlocks/Download_Banner_Desktop.js
@@ -4,7 +4,7 @@ bannerContainerDesktop.innerHTML = `
     <div class="bannerImage bannerImageDesktop">
         <div class="bannerText bannerTextDesktop">
             <div class="bannerSkyrim bannerTextDesktopScale">SKYRIM</div>
-            <div class="bannerDivider bannerTextDesktopScale"></div>
+            <div class="bannerDivider bannerDividerDesktop bannerTextDesktopScale"></div>
             <div class="bannerTogether bannerTextDesktopScale">TOGETHER</div>
             <a href="https://github.com/SkyrimTogether/issues-launcher/releases/" target="_blank"><div class="bannerDownloadButton bannerTextDesktopScale"></div></a>
         </div>
