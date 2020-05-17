ec1a711f6ccf5431fd9514a56e7f9849062e0f1a
thenab0303 <thenab0303@gmail.com>
2020-05-17 08:37:49 -0400

    added desktop download banner

---
+++ CustomBlocks/Download_Banner_Desktop.js
@@ -0,0 +1,12 @@
+const bannerContainerDesktop = document.querySelector(".bannerContainerDesktop");
+
+bannerContainerDesktop.innerHTML = `
+    <div class="bannerImage bannerImageDesktop">
+        <div class="bannerText bannerTextDesktop">
+            <div class="bannerSkyrim">SKYRIM</div>
+            <div class="bannerDivider"></div>
+            <div class="bannerTogether">TOGETHER</div>
+            <a href="https://github.com/SkyrimTogether/issues-launcher/releases/" target="_blank"><div class="bannerDownloadButton"></div></a>
+        </div>
+    </div>
+`;
